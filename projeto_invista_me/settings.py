ed1ed7f3c90c10fdf2f0992ef8dc956292a3df7c
marcosgamasoares <marcosdagamasoares@gmail.com>
2021-04-16 14:35:52 -0300

    Alteração 2 em settings

--- projeto_invista_me/settings.py
+++ projeto_invista_me/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-*$k+p5qz%f5_u@eqk=$)*ka(axlpv4h*_@yxvjm2ikec5mw!3^'
-SECRET_KEY = os.environ('SECRET_KEY')
+SECRET_KEY = os.environ['SECRET_KEY']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
